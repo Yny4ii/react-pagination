698849a991c38052ef696431eff3d3ec6a715b48
Yny4ii <ilya.semyonov.03@mail.ru>
2022-05-25 19:03:17 +0300

    Приложение с пагинацией и поиском новостей

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, } from "@mui/material";
+import HomePage from "./components/HomePage";
+import {Routes, Route} from 'react-router-dom';
+import About from "./components/About";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container sx={{marginTop: 5, maxWidth: "md"}}>
+            <Routes>
+                <Route exact  path="/" element={<HomePage/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+        </Container>
+    );
 }
 
 export default App;
